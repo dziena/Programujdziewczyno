0d29b8826fba5868bad08d2909ab20674486f142
dziena <dziena@o2.pl>
2017-10-21 16:48:06 +0200

    Dodanie linii pod menu

--- js/app.js
+++ js/app.js
@@ -41,4 +41,12 @@ document.addEventListener('DOMContentLoaded', function() {
     buttons[i].addEventListener('click', showHide);
     }
 
+    //zadanie 3 ------
+
+    var navigation = document.querySelector("nav.navbar");
+
+    window.addEventListener("scroll",function(){
+        navigation.style.borderBottom = '#7f7f7f 1px solid';
+    });
+
     });
