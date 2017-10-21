6a21dd0d38509460b839c70fcf403669201b7594
dziena <dziena@o2.pl>
2017-10-21 15:01:37 +0200

    Dodanie rozwijanej listy.

---
+++ js/app.js
@@ -0,0 +1,15 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', function() {
+
+        var menuElement = document.querySelector(".for-dropdown");
+        var dropdownList = document.querySelector(".for-dropdown .dropdown");
+
+
+        menuElement.addEventListener('mouseover', function() {
+            dropdownList.style.display = "block";
+        });
+
+        menuElement.addEventListener('mouseout', function()  {
+            dropdownList.style.display = "none";
+        });
+    });
